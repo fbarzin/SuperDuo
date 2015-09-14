5e535af3d992d2924cbea39559836143ffbe3eec
fbarzin <fbarzin84@gmail.com>
2015-09-14 13:08:27 -0400

    Created data model for Soccer Season, Teams.

---
+++ Football_Scores-master/footballwebapi/src/main/java/com/fbarzin/footballwebapi/model/Team.java
@@ -0,0 +1,17 @@
+package com.fbarzin.footballwebapi.model;
+
+/**
+ * @author farzad
+ * @date 9/14/15
+ */
+public class Team {
+    public Links _links;
+    public String name;
+    public String code;
+    public String shortName;
+    public String squadMarketValue;
+    /**
+     * This is the image URL that is provided by WikiMedia and is in SVG format
+     */
+    public String crestUrl;
+}
--- Football_Scores-master/footballwebapi/src/main/java/com/fbarzin/footballwebapi/model/Teams.java
+++ Football_Scores-master/footballwebapi/src/main/java/com/fbarzin/footballwebapi/model/Teams.java
@@ -1,9 +1,13 @@
 package com.fbarzin.footballwebapi.model;
 
+import java.util.List;
+
 /**
  * @author farzad
  * @date 9/11/15
  */
 public class Teams {
-
+    public Links _links;
+    public int count;
+    public List<Team> teams;
 }
