20e86b0819bc6e7f26513bd2081fc0ae8ae95082
fbarzin <fbarzin84@gmail.com>
2015-09-14 13:26:25 -0400

    Added data model for league table

--- Football_Scores-master/footballwebapi/src/main/java/com/fbarzin/footballwebapi/model/LeagueTable.java
+++ Football_Scores-master/footballwebapi/src/main/java/com/fbarzin/footballwebapi/model/LeagueTable.java
@@ -1,9 +1,14 @@
 package com.fbarzin.footballwebapi.model;
 
+import java.util.List;
+
 /**
  * @author farzad
  * @date 9/11/15
  */
 public class LeagueTable {
-
+    public Links _links;
+    public String leagueCaption;
+    public int matchday;
+    public List<Standing> standing;
 }
---
+++ Football_Scores-master/footballwebapi/src/main/java/com/fbarzin/footballwebapi/model/Standing.java
@@ -0,0 +1,16 @@
+package com.fbarzin.footballwebapi.model;
+
+/**
+ * @author farzad
+ * @date 9/14/15
+ */
+public class Standing {
+    public Links _links;
+    public int position;
+    public String teamName;
+    public int playedGames;
+    public int points;
+    public int goals;
+    public int goalsAgainst;
+    public int goalDifference;
+}
